4d7ca72ab8f9b22319d6602994a8b4e52f44e279
Alan Fineberg <af@cs.washington.edu>
2012-06-08 16:41:47 -0700

    arrow keys can change the duration of the note as long as that note is highlighted

--- js/main.js
+++ js/main.js
@@ -7,23 +7,74 @@
 		$(row).append('|<br>');
 	});
 
-	$('.editable').mouseenter(function (){
+	$('.editable').mouseenter(function () {
 		$(this).addClass('highlighted');
 	});
 
-	$('.editable').mouseleave(function (){
+	$('.editable').mouseleave(function () {
 		$(this).removeClass('highlighted');
 	});
 
-	$('.editable').click(function (){
+	$('.editable').click(function () {
 		$('.selected').removeClass('selected');
 		$(this).addClass('selected');
 		this.textContent = '-';
 	});
 
+	var updateDurationIndicators = function() {
+		var durationIndicators = $('.highlighted').nextUntil('.editable:not(.duration)');
+		var i = 15;
+		durationIndicators.each(function () {
+			$(this).fadeTo(.2, i / 15.0);
+			i -= 1;
+		});
+	};
+	
+	var reduceDuration = function() {
+		var durationIndicators = $('.highlighted').nextUntil('.editable:not(.duration)');
+		var toRemove = durationIndicators.last();
+		toRemove.removeClass('duration');
+		toRemove.removeClass('edited');
+		toRemove.text('-');
+		toRemove.fadeTo(.2, .6);
+
+		updateDurationIndicators();
+	}
+
+	var extendDuration = function() {
+		var durationIndicators = $('.highlighted').nextUntil('.editable:not(.duration)');
+		var nextIndicator = durationIndicators.last().next();
+		nextIndicator.addClass('duration');
+		nextIndicator.text('*');
+
+		updateDurationIndicators();
+	};
+
+	$(document).keyup(function (e) {
+		if (e.keyCode == 37 && $('.highlighted').hasClass('edited')) {
+			reduceDuration();
+		}
+
+		if (e.keyCode == 39 && $('.highlighted').hasClass('edited')) {
+			extendDuration();
+		}
+	});
+
 	$(document).keypress(function (e) {
 		$('.selected').each(function () {
 			this.textContent = e.which - 48;
+			$(this).addClass('edited');
+			$(this).removeClass('duration');
+
+			var nextTick = $(this).next('span');
+			for (var i = 0; i < 3; ++i) {
+				if (nextTick.hasClass('editable')) {
+					nextTick.addClass('duration');
+					nextTick.text('*');
+					nextTick = nextTick.next('span');
+				}
+			}
+			updateDurationIndicators();
 		});
 
 		$('.selected').removeClass('selected');
